362c4a3b8a19b34ebfe2e6fbf636b6340a73630b
darakolesnikova <dkolesnikova1999@bk.ru>
2022-12-29 17:58:37 +0500

    add new test

---
+++ src/test/java/guru/qa/CheckUploadPageTest.java
@@ -0,0 +1,29 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.text;
+
+
+
+public class CheckUploadPageTest  {
+    @Test
+    void pageOpeningCheck () {
+    open ("https://github.com");
+    $$(".HeaderMenu-link").findBy(text("Solutions")).hover();
+    $$(".HeaderMenu-dropdown-link").findBy(text("Enterprise")).click();
+    $(".enterprise-hero").shouldHave(text("Build like the best"));
+    sleep(5000);
+    }
+
+    @Test
+    void dragAndDrop () {
+    open("https://the-internet.herokuapp.com/drag_and_drop");
+    $("#column-a").dragAndDropTo($("#column-b"));
+    $("#column-b header").shouldHave(text("A"));
+    $("#column-a header").shouldHave(text("B"));
+    }
+
+}
+
